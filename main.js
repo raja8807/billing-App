60602d05a6506c77421c2ca28c10467d6665d615
Raja <rajarathinam1999@gmail.com>
2022-07-29 16:08:24 +0530

    29.7

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// alert("ok")
+let btns = document.querySelectorAll(".calculateBtn")
+
+function calculate(){
+    let qty = parseInt(this.parentNode.previousElementSibling.firstChild.value);
+    let price = parseInt(this.parentNode.previousElementSibling.previousElementSibling.firstChild.value);
+    let amount = qty * price;
+    console.log(amount);
+    this.parentNode.nextElementSibling.firstChild.innerText = amount;
+}
+
+function addEvent(btn) {
+    btn.addEventListener("click" , calculate)
+}
+
+btns.forEach(addEvent)
