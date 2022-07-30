a4c39fe686eacb66473dee8518eb3168417a4cb5
Raja <rajarathinam1999@gmail.com>
2022-07-30 11:55:31 +0530

    30.7

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-let btns = document.querySelectorAll(".calculateBtn")
+// let btns = document.querySelectorAll(".calculateBtn");
+// function calculate(){
+//     let qty = parseInt(this.parentNode.previousElementSibling.firstChild.value);
+//     let price = parseInt(this.parentNode.previousElementSibling.previousElementSibling.firstChild.value);
+//     let amount = qty * price;
+//     this.parentNode.nextElementSibling.firstChild.innerText = amount;
+// }
+// btns.forEach((btn)=>{
+//     btn.addEventListener("click" , calculate)
+// });
+
+//.................................................................
+
+let btns = document.querySelectorAll(".calculateBtn");
+let prices = document.querySelectorAll(".price")
+let qntys = document.querySelectorAll(".qnty")
+let amnt = document.querySelectorAll(".amt")
+let totalOut = document.getElementById("total")
+let total = 0;
 
 function calculate(){
-    let qty = parseInt(this.parentNode.previousElementSibling.firstChild.value);
-    let price = parseInt(this.parentNode.previousElementSibling.previousElementSibling.firstChild.value);
-    let amount = qty * price;
-    console.log(amount);
-    this.parentNode.nextElementSibling.firstChild.innerText = amount;
+    let amt = prices[this.i].value * qntys[this.i].value;
+    amnt[this.i].innerText = amt;
+    total = total + amt;
+    totalOut.innerText = total;
 }
-
-function addEvent(btn) {
+btns.forEach((btn,index)=>{
     btn.addEventListener("click" , calculate)
-}
+    btn.i = index;
+});
 
-btns.forEach(addEvent)
+//...............................................
